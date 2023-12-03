247aecf22623d0885f576ca729eafb586e695383
vonHousen <42911623+vonHousen@users.noreply.github.com>
2023-12-03 13:01:26 +0100

    Refactoring.

--- day#3/part1.py
+++ day#3/part1.py
@@ -18,7 +18,6 @@ VECTORS_TO_CHECK_ADJACENCY = [
 @dataclasses.dataclass(frozen=True)
 class PartNumber:
     value: int
-    number_id: int
 
     def __repr__(self) -> str:
         return str(self.value)
@@ -35,48 +34,44 @@ def sum_all_part_numbers() -> None:
     with open("input.txt", "r") as f:
         engine_schematic = f.read().splitlines()
 
-    part_numbers_sum = sum(__get_all_part_numbers(engine_schematic))
+    part_numbers_sum = sum(part_number.value
+                           for matched_symbol_adjacent_numbers in _get_all_part_numbers(engine_schematic,
+                                                                                        re.compile(r"[^\d\.]"))
+                           for part_number in matched_symbol_adjacent_numbers)
 
     print(f"The sum of all of the part numbers in the engine schematic: {part_numbers_sum}.")
 
 
-def __get_all_part_numbers(engine_schematic: list[str]) -> list[int]:
+def _get_all_part_numbers(engine_schematic: list[str],
+                          symbol_regex: re.Pattern) -> list[set[PartNumber]]:
     numbers_occurrence_2d_map = __get_numbers_occurrence_2d_map(engine_schematic)
 
-    symbol_regex = re.compile(r"[^\d\.]")
-    adjacent_numbers: list[PartNumber] = []
+    all_adjacent_numbers: list[set[PartNumber]] = []
     for row_idx, line in enumerate(engine_schematic):
         for matched_symbol in symbol_regex.finditer(line):
             col_idx = matched_symbol.start()
-            last_part_number_id = None
+            matched_symbol_adjacent_numbers: set[PartNumber] = set()
             for row_idx_addend, col_idx_addend in VECTORS_TO_CHECK_ADJACENCY:
                 found_number = numbers_occurrence_2d_map.get((row_idx + row_idx_addend, col_idx + col_idx_addend))
-                if found_number is None:
-                    continue
-                if found_number.number_id == last_part_number_id:
-                    continue
+                if found_number is not None:
+                    matched_symbol_adjacent_numbers.add(found_number)
 
-                adjacent_numbers.append(found_number)
-                last_part_number_id = found_number.number_id
+            all_adjacent_numbers.append(matched_symbol_adjacent_numbers)
 
-    return [part_number.value for part_number in adjacent_numbers]
+    return all_adjacent_numbers
 
 
 def __get_numbers_occurrence_2d_map(engine_schematic: Iterable[str]) -> dict[tuple[int, int], PartNumber]:
     number_regex = re.compile(r"\d+")
-    part_number_id = 0
     numbers_occurrence_2d_map: dict[tuple[int, int], PartNumber] = {}
 
     for row_idx, line in enumerate(engine_schematic):
         for matched_symbol in number_regex.finditer(line):
-            number = int(matched_symbol[0])
-            potential_part_number = PartNumber(value=number, number_id=part_number_id)
+            potential_part_number = PartNumber(value=int(matched_symbol[0]))
 
             start_idx, end_idx = matched_symbol.start(), matched_symbol.end()
             for col_idx in range(start_idx, end_idx):
                 numbers_occurrence_2d_map[row_idx, col_idx] = potential_part_number
-
-            part_number_id += 1
 
     return numbers_occurrence_2d_map
 
--- day#3/part2.py
+++ day#3/part2.py
@@ -1,6 +1,7 @@
 import re
+from math import prod
 
-from part1 import __get_numbers_occurrence_2d_map, VECTORS_TO_CHECK_ADJACENCY, PartNumber
+from part1 import _get_all_part_numbers
 
 
 def sum_gear_ratios() -> None:
@@ -13,35 +14,12 @@ def sum_gear_ratios() -> None:
     with open("input.txt", "r") as f:
         engine_schematic = f.read().splitlines()
 
-    gear_ratios_sum = sum(__get_all_gear_ratios(engine_schematic))
+    gear_ratios = [prod(part_number.value for part_number in matched_symbol_adjacent_numbers)
+                   for matched_symbol_adjacent_numbers in _get_all_part_numbers(engine_schematic, re.compile(r"[\*]"))
+                   if len(matched_symbol_adjacent_numbers) == 2]
+    gear_ratios_sum = sum(gear_ratios)
 
     print(f"The sum of all of the gear ratios: {gear_ratios_sum}.")
-
-
-def __get_all_gear_ratios(engine_schematic: list[str]) -> list[int]:
-    numbers_occurrence_2d_map = __get_numbers_occurrence_2d_map(engine_schematic)
-
-    symbol_regex = re.compile(r"\*")
-    gear_ratios: list[int] = []
-    for row_idx, line in enumerate(engine_schematic):
-        for matched_symbol in symbol_regex.finditer(line):
-            col_idx = matched_symbol.start()
-            last_part_number_id = None
-            adjacent_numbers: list[PartNumber] = []
-            for row_idx_addend, col_idx_addend in VECTORS_TO_CHECK_ADJACENCY:
-                found_number = numbers_occurrence_2d_map.get((row_idx + row_idx_addend, col_idx + col_idx_addend))
-                if found_number is None:
-                    continue
-                if found_number.number_id == last_part_number_id:
-                    continue
-
-                adjacent_numbers.append(found_number)
-                last_part_number_id = found_number.number_id
-
-            if len(adjacent_numbers) == 2:
-                gear_ratios.append(adjacent_numbers[0].value * adjacent_numbers[1].value)
-
-    return gear_ratios
 
 
 if __name__ == "__main__":
